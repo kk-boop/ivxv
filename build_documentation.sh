baac7e84d7df8d2a2672f42eee7e637dbdfc4ce2
kk-boop <56920559+kk-boop@users.noreply.github.com>
2020-04-11 19:26:20 +0300

    Add Github action for documentation build on release

--- build_documentation.sh
+++ build_documentation.sh
@@ -23,3 +23,11 @@ mv ./*.pdf $dir/output/et/
 cd $dir/output
 zip -j et_documentation.zip et/*
 zip -j en_documentation.zip en/*
+
+if [ ! -z "$GITHUB_TOKEN" ];
+then
+
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Content-Length: $(stat -c%s "et_documentation.zip")" -H "Content-Type: application/zip" --upload-file et_documentation.zip "https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=et_documentation.zip"
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Content-Length: $(stat -c%s "en_documentation.zip")" -H "Content-Type: application/zip" --upload-file en_documentation.zip "https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=en_documentation.zip"
+
+fi
